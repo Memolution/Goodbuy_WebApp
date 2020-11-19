990ea44f08c111f333a3ba1495ffecdefc409421
Sydney <s1811552@s.tsukuba.ac.jp>
2020-11-20 06:12:56 +0900

    質問が見られるようになった

--- frontend/src/components/viewQuestion.vue
+++ frontend/src/components/viewQuestion.vue
@@ -1,22 +1,38 @@
 <template>
 <div>
   <v-app>
-
+    <!-- <h1>{{questionData}}</h1> -->
+    <p v-for="data in questionData" :key='data.id'>
+      <v-card light elevation="16">
+        <!-- <v-card-title class="headline">考えてみよう</v-card-title> -->
+        <!-- <v-card-subtitle>card subtitle</v-card-subtitle> -->
+        <v-divider class="mx-3"></v-divider>
+        <v-card-text class=text-center>
+          <div class="body-1 mb-1">
+             <h2>{{data.content}}</h2>
+          </div>
+        </v-card-text>
+        <v-card-actions class="justify-center">
+          <!-- <v-spacer></v-spacer> -->
+          <v-btn large>大丈夫！</v-btn>
+          <v-btn large>考え直す</v-btn>
+        </v-card-actions>
+      </v-card>
+    </p>
   </v-app>
 </div>
 </template>
 
 <script>
-// export default {
-//   data () {
-//     return {
-//       text: {
-//
-//       }
-//     }
-//   },
-//   methods: {
-//     getQuestion
-//   }
-// }
+export default {
+  data () {
+    return {
+    }
+  },
+  props: {
+    questionData: {
+      type: Array
+    }
+  }
+}
 </script>
