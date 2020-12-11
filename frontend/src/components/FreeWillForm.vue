c569e5ccd3b039d44f1a9f827d7223520d53bd3c
Sydney <s1811552@s.tsukuba.ac.jp>
2020-12-11 17:09:43 +0900

    モブプロ

--- frontend/src/components/FreeWillForm.vue
+++ frontend/src/components/FreeWillForm.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <v-app>
-        <v-card>
+      <v-card>
         <v-card-title>購入理由を自由記述で書いてみよう</v-card-title>
         <details>
           <summary>記入例</summary>
-          原付と黄色のヘルメットを買いたいと思います。活動範囲が広くなるからです。一人で海に行ったりしたいから、買います。車は、高いしお金がかかるので、原付にしました。二人乗りは、免許を取り直す必要があり、少しだるい。だから原付。
+          原付と黄色のヘルメットを買いたいと思います。<br />
+          【商品によって解決できる課題】活動範囲が広くなるからです。<br />
+          【商品が課題解決にもたらす効果】移動速度と移動距離が飛躍的に伸びます。<br />
+          【代替可能な商品との比較】車は、高いしお金がかかるので、原付にしました。<br />
+          【その他の気持ち】原付二種は、免許を取り直す必要があって、少しだるい。<br />
+          だから原付。
         </details>
         <!-- <br /> -->
         <v-textarea
@@ -15,12 +20,9 @@
           v-model="tweetContent.tweetWhy"
           label="購買動機を入力してね"
         >
-        >
+          >
         </v-textarea>
-        <v-text-field
-          v-model="recentUrl"
-          label="商品のURLを入力してね"
-        >
+        <v-text-field v-model="recentUrl" label="商品のURLを入力してね">
         </v-text-field>
         <v-divider></v-divider>
         <v-card-actions>
@@ -28,75 +30,86 @@
             <v-icon>mdi-twitter</v-icon>
             ツイートする
           </v-btn>
+          <v-btn text v-on:click="show_message">
+            <v-icon>mdi-account-check-outline</v-icon>
+            完了
+          </v-btn>
         </v-card-actions>
       </v-card>
       <div v-if="this.validation.validateResult == 'めっちゃいい理由！'">
-        <v-alert
-        outlined
-        type="success"
-        prominent
-        border="left"
-        >
-        {{validation.validateResult}}
+        <v-alert outlined type="success" prominent border="left">
+          {{ validation.validateResult }}
         </v-alert>
       </div>
-      <div v-else-if=" this.validation.validateResult == '20字以上入力してください' || this.validation.validateResult == '熱入りすぎだよ！' ">
-        <v-alert
-        outlined
-        type="warning"
-        prominent
-        border="left"
-        >
-        {{validation.validateResult}}
+      <div
+        v-else-if="
+          this.validation.validateResult == '20字以上入力してください' ||
+          this.validation.validateResult == '熱入りすぎだよ！'
+        "
+      >
+        <v-alert outlined type="warning" prominent border="left">
+          {{ validation.validateResult }}
         </v-alert>
       </div>
+      <!-- <div v-if="this.message.length > 0">
+        <v-alert outlined type="success" prominent border="left">
+          {{ message }}
+        </v-alert>
+      </div> -->
     </v-app>
   </div>
-
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   name: 'willForm',
   data: function () {
     return {
       tweetContent: {
-        tweetWhy: ''
+        tweetWhy: '',
       },
       validation: {
-        validateResult: ''
+        validateResult: '',
       },
-      recentUrl: []
-    }
+      recentUrl: [],
+      message: '',
+    };
   },
   methods: {
-    GetTweet (str, code) {
+    GetTweet(str, code) {
       if (this.tweetContent.tweetWhy.length < 20) {
-        this.validation.validateResult = '20字以上入力してください'
+        this.validation.validateResult = '20字以上入力してください';
       } else if (this.tweetContent.tweetWhy.length > 120) {
-        this.validation.validateResult = '熱入りすぎだよ！'
+        this.validation.validateResult = '熱入りすぎだよ！';
       } else {
-        this.validation.validateResult = 'めっちゃいい理由！'
-        var target = document.getElementById('TWEET')
-        var textAll = this.tweetContent.tweetWhy + this.recentUrl
-        var inputData = textAll.replace(/\r?\n/g, '%0D%0A')
-        var path = 'https://twitter.com/intent/tweet?hashtags=Goodbuy_enp&text=' + inputData
-        target.innerHTML = '<a href=' + path + '>Tweet</a>'
+        this.validation.validateResult = 'めっちゃいい理由！';
+        var target = document.getElementById('TWEET');
+        var textAll = this.tweetContent.tweetWhy + this.recentUrl;
+        var inputData = textAll.replace(/\r?\n/g, "%0D%0A");
+        var path =
+          'https://twitter.com/intent/tweet?hashtags=Goodbuy_enp&text=' +
+          inputData;
+        target.innerHTML = '<a href=' + path + '>Tweet</a>';
       }
-    }
+    },
+    show_message() {
+      // this.message = 'お疲れ様！';
+      alert('お疲れ様でした！このタブは閉じても大丈夫です。')
+    },
   },
-  created () {
-    this.recentUrl = []
-    var isUrl = this.$route.path
-    var wasUrl = isUrl.split('will/')[1]
-    this.recentUrl.push(wasUrl)
-  }
-}
+  created() {
+    this.recentUrl = [];
+    var isUrl = this.$route.path;
+    var wasUrl = isUrl.split('will/')[1];
+    this.recentUrl.push(wasUrl);
+  },
+};
 </script>
 
 <style lang="scss">
 .tweetBox {
-  border: 2px solid #00aced ;
+  border: 2px solid #00aced;
   border-radius: 0.67em;
   padding: 0.5em;
   background-color: snow;
@@ -106,43 +119,45 @@ export default {
   line-height: 1.2;
 }
 #TWEET {
-  width:150px;
-  height:30px;
-  line-height:30px;
-  text-align:center;
+  width: 150px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
 
   background-color: #55acee;
   border: 2px solid #55acee;
   border-radius: 20px;
   color: #fff;
-  padding:4px 32px;
-  -webkit-transition: all .3s;
-  transition: all .3s;
+  padding: 4px 32px;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
 }
 #TWEET:hover {
   background-color: #fff;
   color: #55acee;
 }
-.error { color: red; }
+.error {
+  color: red;
+}
 
 details {
-    border: 1px solid #aaa;
-    border-radius: 4px;
-    padding: .5em .5em 0;
+  border: 1px solid #aaa;
+  border-radius: 4px;
+  padding: 0.5em 0.5em 0;
 }
 
 summary {
-    font-weight: bold;
-    margin: -.5em -.5em 0;
-    padding: .5em;
+  font-weight: bold;
+  margin: -0.5em -0.5em 0;
+  padding: 0.5em;
 }
 
 details[open] {
-    padding: .5em;
+  padding: 0.5em;
 }
 
 details[open] summary {
-    border-bottom: 1px solid #aaa;
-    margin-bottom: .5em;
+  border-bottom: 1px solid #aaa;
+  margin-bottom: 0.5em;
 }
 </style>
--- frontend/src/components/WillForm.vue
+++ frontend/src/components/WillForm.vue
@@ -2,30 +2,31 @@
   <div>
     <v-app>
       <v-card>
-        <v-card-title>購入理由をエレベーターピッチ風に書いてみよう</v-card-title>
+        <v-card-title
+          >購入理由をエレベーターピッチ風に書いてみよう</v-card-title
+        >
         <v-card-text>
-
           <br />
-          <h3>{{evp_template.temp1}}</h3>
+          <h3>{{ evp_template.temp1 }}</h3>
 
           <v-text-field
             v-model="tweetContent.tweetWhy"
             label="購買動機を入力してね"
-            >
+          >
           </v-text-field>
 
-          <h3>{{evp_template.temp2}}</h3>
+          <h3>{{ evp_template.temp2 }}</h3>
           <br />
-          <h3>{{evp_template.temp3}}</h3>
+          <h3>{{ evp_template.temp3 }}</h3>
 
           <v-text-field
-          v-model="tweetContent.tweetWhat"
-          label="どんな問題が解決できるか入力してね"
+            v-model="tweetContent.tweetWhat"
+            label="どんな問題が解決できるか入力してね"
           >
           </v-text-field>
 
           <br />
-          <h3>{{evp_template.temp4}}</h3>
+          <h3>{{ evp_template.temp4 }}</h3>
           <v-text-field
             v-model="tweetContent.tweetHow"
             label="どのようにして解決できそうか入力してね"
@@ -33,12 +34,9 @@
           </v-text-field>
 
           <br />
-          <h3>{{evp_template.temp5}}</h3>
+          <h3>{{ evp_template.temp5 }}</h3>
         </v-card-text>
-        <v-text-field
-          v-model="recentUrl"
-          label="商品のURLを入力してね"
-        >
+        <v-text-field v-model="recentUrl" label="商品のURLを入力してね">
         </v-text-field>
         <v-divider></v-divider>
         <v-card-actions>
@@ -46,33 +44,38 @@
             <v-icon>mdi-twitter</v-icon>
             ツイートする
           </v-btn>
+          <v-btn text v-on:click="show_message">
+            <v-icon>mdi-account-check-outline</v-icon>
+            完了
+          </v-btn>
         </v-card-actions>
       </v-card>
       <div v-if="this.validation.validateResult == 'めっちゃいい理由！'">
-        <v-alert
-        outlined
-        type="success"
-        prominent
-        border="left"
-        >
-        <h2>{{validation.validateResult}}</h2>
+        <v-alert outlined type="success" prominent border="left">
+          <h2>{{ validation.validateResult }}</h2>
         </v-alert>
       </div>
-      <div v-else-if=" this.validation.validateResult == '20字以上入力してください' || this.validation.validateResult == '熱入りすぎだよ！' ">
-        <v-alert
-        outlined
-        type="warning"
-        prominent
-        border="left"
-        >
-        <h2>{{validation.validateResult}}</h2>
+      <div
+        v-else-if="
+          this.validation.validateResult == '20字以上入力してください' ||
+          this.validation.validateResult == '熱入りすぎだよ！'
+        "
+      >
+        <v-alert outlined type="warning" prominent border="left">
+          <h2>{{ validation.validateResult }}</h2>
         </v-alert>
       </div>
+      <!-- <div v-if="this.message.length > 0">
+        <v-alert outlined type="success" prominent border="left">
+          {{ message }}
+        </v-alert>
+      </div> -->
     </v-app>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   name: 'willForm',
   data: function () {
@@ -82,49 +85,76 @@ export default {
         temp2: 'を解決する事を目的に買います。',
         temp3: 'これがあると',
         temp4: 'を',
-        temp5: 'して問題を解決できます。',
+        temp5: '(して)問題を解決できます。',
         temp6: 'これは、今の私の所持品では代替',
-        temp7: '。'
+        temp7: '。',
       },
       tweetContent: {
         tweetWhy: '',
         tweetWhat: '',
-        tweetHow: ''
+        tweetHow: '',
       },
       validation: {
-        validateResult: ''
+        validateResult: '',
       },
-      recentUrl: []
-    }
+      recentUrl: [],
+      message: ''
+    };
   },
   methods: {
     GetTweet (str, code) {
-      if (this.tweetContent.tweetWhy.length + this.tweetContent.tweetWhat.length + this.tweetContent.tweetHow.length < 20) {
-        this.validation.validateResult = '20字以上入力してください'
-      } else if (this.tweetContent.tweetWhy.length + this.tweetContent.tweetWhat.length + this.tweetContent.tweetHow.length > 87) {
-        this.validation.validateResult = '熱入りすぎだよ！'
+      if (
+        this.tweetContent.tweetWhy.length +
+          this.tweetContent.tweetWhat.length +
+          this.tweetContent.tweetHow.length <
+        20
+      ) {
+        this.validation.validateResult = '20字以上入力してください';
+      } else if (
+        this.tweetContent.tweetWhy.length +
+          this.tweetContent.tweetWhat.length +
+          this.tweetContent.tweetHow.length >
+        87
+      ) {
+        this.validation.validateResult = '熱入りすぎだよ！';
       } else {
-        this.validation.validateResult = 'めっちゃいい理由！'
-        var target = document.getElementById('TWEET')
-        var textAll = this.evp_template.temp1 + this.tweetContent.tweetWhy + this.evp_template.temp2 + this.evp_template.temp3 + this.tweetContent.tweetWhat + this.evp_template.temp4 + this.tweetContent.tweetHow + this.evp_template.temp5 + this.recentUrl
-        var inputData = textAll.replace(/\r?\n/g, '%0D%0A')
-        var path = 'https://twitter.com/intent/tweet?hashtags=Goodbuy_enp&text=' + inputData
-        target.innerHTML = '<a href=' + path + '>Tweet</a>'
+        this.validation.validateResult = 'めっちゃいい理由！';
+        var target = document.getElementById('TWEET');
+        var textAll =
+          this.evp_template.temp1 +
+          this.tweetContent.tweetWhy +
+          this.evp_template.temp2 +
+          this.evp_template.temp3 +
+          this.tweetContent.tweetWhat +
+          this.evp_template.temp4 +
+          this.tweetContent.tweetHow +
+          this.evp_template.temp5 +
+          this.recentUrl;
+        var inputData = textAll.replace(/\r?\n/g, '%0D%0A');
+        var path =
+          'https://twitter.com/intent/tweet?hashtags=Goodbuy_enp&text=' +
+          inputData;
+        target.innerHTML = '<a href=' + path + '>Tweet</a>';
       }
-    }
+    },
+    show_message () {
+      // this.message = "お疲れ様でした!"
+      alert('お疲れ様でした！このタブは閉じても大丈夫です。')
+
+    },
   },
   created () {
-    this.recentUrl = []
-    var isUrl = this.$route.path
-    var wasUrl = isUrl.split('will/')[1]
-    this.recentUrl.push(wasUrl)
-  }
-}
+    this.recentUrl = [];
+    var isUrl = this.$route.path;
+    var wasUrl = isUrl.split('will/')[1];
+    this.recentUrl.push(wasUrl);
+  },
+};
 </script>
 
 <style lang="scss">
 .tweetBox {
-  border: 2px solid #00aced ;
+  border: 2px solid #00aced;
   border-radius: 0.67em;
   padding: 0.5em;
   background-color: snow;
@@ -134,22 +164,24 @@ export default {
   line-height: 1.2;
 }
 #TWEET {
-  width:150px;
-  height:30px;
-  line-height:30px;
-  text-align:center;
+  width: 150px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
 
   background-color: #55acee;
   border: 2px solid #55acee;
   border-radius: 20px;
   color: #fff;
-  padding:4px 32px;
-  -webkit-transition: all .3s;
-  transition: all .3s;
+  padding: 4px 32px;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
 }
 #TWEET:hover {
   background-color: #fff;
   color: #55acee;
 }
-.error { color: red; }
+.error {
+  color: red;
+}
 </style>
