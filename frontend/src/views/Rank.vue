4cdd0423daea685ecf9ce019710f8276e357778f
Sydney <s1811552@s.tsukuba.ac.jp>
2021-01-09 12:22:54 +0900

    Create Rank.vue

---
+++ frontend/src/views/Rank.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-card>
+      <v-card-title>
+        どれくらい買い物習慣が上手くなったか確認しよう
+      </v-card-title>
+      <v-text-field>
+      </v-text-field>
+
+      <br />
+    </v-card>
+    <ol id="pyramid">
+      <li>王</li>
+      <li>王直属護衛軍</li>
+      <li>師団長</li>
+      <li>兵隊長</li>
+      <li>戦闘兵</li>
+    </ol>
+    <!-- <apexchart
+  type="area"
+  height="400"
+  :options="chart.options"
+  :series="chart.series"
+></apexchart> -->
+  </v-app>
+</template>
+
+<!-- <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script src="vue-fusioncharts.js" type="text/javascript"></script> -->
+<script>
+/* eslint-disable */
+</script>
