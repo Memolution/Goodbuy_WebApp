dcc0bbdb4a23a0b3fd33a6d249566eb0aa0f300a
Sydney <s1811552@s.tsukuba.ac.jp>
2021-01-09 16:23:51 +0900

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -200,3 +200,19 @@ def level_up():
         message = {'message': comment}
 
     return jsonify(message)
+
+
+@api.route("/viewLevel", methods=["POST"])
+def view_level():
+    degree = ['初心者', '見習い', '一人前', '達人']
+    visit_count = request.get_json()['visitCount']['count']
+    current_level = int(visit_count / 5)
+    times = 5 - int(visit_count % 5)
+
+    if current_level >= len(degree):
+        message = {'message':'いい買い物習慣の達人の域です!'}
+    else:
+        comment = 'いい買い物習慣のレベルはずばり{0}です！\n あと{1}回購入理由を考えると買い物習慣が上達します！'.format(degree[current_level], times)
+    message = {'message': comment}
+
+    return jsonify(message)
